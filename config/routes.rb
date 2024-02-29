c54976b4b9810546f31e0377c1880a1012b8ccba
Mia Dinh <miadinh38@gmail.com>
2024-02-29 02:44:07 -0500

    Declare new routes for login & signup

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,15 @@ Rails.application.routes.draw do
 
   get '/about', to: 'about#index'
 
+  # Routes for signup
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  # Routes for login
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
