7358f7bc75efacfe8c62cca10c35438f8f8cff3f
Mia Dinh <miadinh38@gmail.com>
2024-03-03 21:51:42 -0500

    Define validations for User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
 
   has_secure_password
-  
+
+  validates :password, presence: true, length: { minimum: 5 }
+  validates :password_confirmation, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :name, presence: true
 end
